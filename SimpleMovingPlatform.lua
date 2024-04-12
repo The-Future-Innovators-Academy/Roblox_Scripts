26c512b38fb1d89418be193d5f5415e8cd2eb521
Satyadev Moolagani <116038307+srmoola@users.noreply.github.com>
2024-04-12 16:21:43 -0400

    Create SimpleMovingPlatform.lua

---
+++ SimpleMovingPlatform.lua
@@ -0,0 +1,22 @@
+local model = script.Parent
+local anchor = model.Anchor
+local start = model.Start
+local finish = model.Finish
+
+-- Distance the platform will move
+
+local distance = finish.Position - start.Position
+local travelTime = 360
+local incrementX = distance.X/travelTime
+local incrementY = distance.Y/travellime
+local incrementZ = distance.ZtravelTime
+local timer = 0
+
+—- Moves the platform
+
+function move ()
+  timer += 1
+  anchor.Position += Vector.new(incrementX, incrementY, incrementz)
+and
+  
+game:GetService("RunService”).Heartbeat:Connect(move)
